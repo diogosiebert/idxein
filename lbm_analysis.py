a679cc03c32db324e75fd613f26a6bec51b429a4
Diogo Nardelli Siebert <diogo.siebert@ufsc.br>
2023-12-07 23:14:32 -0300

    Trying to perform second order analysis with temperature

--- lbm_analysis.py
+++ lbm_analysis.py
@@ -58,12 +58,13 @@ J = lambda b, n = None  : ( m if n == None else sp.Matrix( m[:n] ) ).jacobian(  
 
 n = len(m)
 MLJ = simplifyKronecker( M(n-1) @ L(n) @ J( b(1) , n  ) ).subs( { dk( b(1), a(1)) : 1 } ).xreplace( { b(1) : a(1) } )
-MUJ = simplifyKronecker( M(n-1) @ U(n) @ J( b(1) , n  ) ).subs( { dk( b(1), a(1)) : 1 } ).xreplace( { b(1) : a(1) } )
+#MUJ = simplifyKronecker( M(n-1) @ U(n) @ J( b(1) , n  ) ).subs( { dk( b(1), a(1)) : 1 } ).xreplace( { b(1) : a(1) } )
+MUJ = simplifyKronecker( M(n-1) @ U(n) @ J( b(1) , n  ) )
 TG = lambda bt = b(1), et = e(1) : simplifyKronecker( - MLJ.inv() @ MUJ ).xreplace( { a(1) : bt, e(1) : et } )
 
 grad = lambda e,b : sp.Matrix( [ D(v, x[e] ) for v in var(b) ] )
 dt   = lambda b : sp.Matrix( [ D(v,t) for v in var( b ) ] )
 
 sp.Eq( dt(b(1)), simplifyKronecker( TG(b(1)) @ grad( e(1), b(1) ) ) )
-
-
+grad = D( sp.Matrix( [ rho, u[b(1)] , tt ] ) , x[ e(1) ]  )
+U( 4, e(2) ) @ simplifyKronecker( ( - L(5) @ J(b(2), 5) @ simplifyKronecker( (MLJ.inv() @ MUJ ) ).xreplace( { a(1) : b(2) } ) + U( 5, e(1) ) @ J( b(1), 5 ) ) @ grad )
